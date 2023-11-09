6639ac95d3d4c5e7fc2e01c16898bd3742b6e9fa
Mara <santamaria.ortiz.tamara@gmail.com>
2023-11-09 13:34:08 +0100

    1.4 done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,20 +3,28 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
-  return <div>...</div>;
+    return (
+      <div>
+        <Header course={course} />
+        <Content parts={parts} />
+        <Total parts={parts} />
+      </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
