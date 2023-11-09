d40521670140ea91ee8ab1ea6ecaba76823f075c
Mara <santamaria.ortiz.tamara@gmail.com>
2023-11-09 13:32:48 +0100

    1.3. done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
-  return (
-    <div>
-      <Part ... />
-      <Part ... />
-      <Part ... />
-    </div>
-  );
+  return <div>...</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
