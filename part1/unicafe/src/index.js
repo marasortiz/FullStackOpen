37312bac5f934567600747b51806d7a34317750a
Mara <santamaria.ortiz.tamara@gmail.com>
2023-11-13 18:23:50 +0100

    1.6. done v1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,13 +1,59 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+//const App = () => {
+// save clicks of each button to its own state
+//  const [good, setGood] = useState(0);
+//  const [neutral, setNeutral] = useState(0);
+//  const [bad, setBad] = useState(0);
+
+//  return <div>code here</div>;
+//};
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
 const App = () => {
-  // save clicks of each button to its own state
+  const [value, setValue] = useState(10);
+
+  
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  
+  const setToGood = (newValue) => {
+    setGood(newValue);
+  };
+  const setToNeutral = (newValue) => {
+    setNeutral(newValue);
+  };
+  const setToBad = (newValue) => {
+    setBad(newValue);
+  };
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <h1>Stadistics</h1>
+      <div>
+        good
+        <Display value={good} />
+      </div>
+      <div>
+        bad
+        <Display value={neutral} />
+      </div>
+      <div>
+        nretral
+        <Display value={bad} />
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
