70573d23584d59f99ecc34b28e8073f27523c5b4
Mara <santamaria.ortiz.tamara@gmail.com>
2023-11-14 13:54:41 +0100

    1.14. done (Step 3)

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,24 +1,49 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = (props) => {
   const [selected, setSelected] = useState(-1);
+  const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0));
 
   const getRandomAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * props.anecdotes.length);
     setSelected(randomIndex);
   };
 
+  const handleVote = () => {
+    const updatedVotes = [...votes];
+    updatedVotes[selected] += 1;
+    setVotes(updatedVotes);
+  };
+
+  const getMaxVoted = () => {
+    return votes.indexOf(Math.max(...votes));
+  };
+
+  const maxVotedIndex = getMaxVoted();
+
   if (selected === -1) {
-    return (<button onClick={getRandomAnecdote}>Show A Random Anecdote</button>)
+    return (
+      <div>
+        <button onClick={getRandomAnecdote}>Show A Random Anecdote</button>
+      </div>
+    );
   }
 
   return (
     <div>
-      <div>{props.anecdotes[selected]}</div>
+      <div>
+        <h1>Anecdote of the day</h1>
+        <p>{props.anecdotes[selected]}</p>
+        <p>Votes: {votes[selected]}</p>
+      </div>
+      <button onClick={handleVote}>Vote for this Anecdote</button>
       <button onClick={getRandomAnecdote}>Show Another Random Anecdote</button>
+      <div>
+        <h1>Anecdote of the day</h1>
+        <p>{props.anecdotes[maxVotedIndex]}</p>
+        <p>Votes: {votes[maxVotedIndex]}</p>
+      </div>
     </div>
   );
 };
@@ -32,8 +57,7 @@ const anecdotes = [
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App anecdotes={anecdotes} />
