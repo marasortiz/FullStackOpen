f653224da2ee3be97da30e107aa61ec18589cfcb
Mara <santamaria.ortiz.tamara@gmail.com>
2023-11-27 12:57:06 +0100

    2.11. done (phonebook part6)

--- part2/phonebook/src/Appv1.js
+++ part2/phonebook/src/Appv1.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Filter from "./components/filter";
 import PhoneBook from "./components/phonebook";
-
-import axios from "axios";
 
 import "./App.css";
 
@@ -29,21 +27,17 @@ const PersonForm = ({
 };
 
 const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
 
-  const [persons, setPersons] = useState([]);
-  
   const [newName, setNewName] = useState("enter name...");
   const [newNumber, setNewNumber] = useState("enter number...");
   const [newId, setNewId] = useState("");
   const [filter, setFilter] = useState("");
-  
-  useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
-      console.log("promise fulfilled");
-      setPersons(response.data);
-    });
-  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import axios from "axios";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +12,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/* axios.get("http://localhost:3001/persons").then((response) => {
+  const persons = response.data;
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App persons={persons} />
+    </React.StrictMode>
+  );
+
+}); */
