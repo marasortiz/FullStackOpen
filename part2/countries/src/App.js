4acef44f5d098d77fc1f7409fcd2387a49cdb7e9
Mara <santamaria.ortiz.tamara@gmail.com>
2023-11-27 13:46:10 +0100

    2.12. v1 done (RestCountries part1)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [filterCountries, setFilterCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    setFilterCountries(
+      countries.filter(
+        (country) =>
+          country.name.common.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+      )
+    );
+  }, [countries, filter]);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        filter shown with{" "}
+        <input onChange={handleFilterChange} value={filter}></input>
+      </div>
+      {filterCountries.length <= 10 ? (
+        filterCountries.length === 1 ? (
+          filterCountries.map((e) => {
+            return (
+              <div key={e.cca3}>
+                <h1>{e.name.common}</h1>
+                <p>capital {e.capital}</p>
+                <p>population {e.population}</p>
+                <h3>languages</h3>
+                <ul>
+                  {Object.values(e.languages).map((lang, i) => (
+                    <li key={i}>{lang}</li>
+                  ))}
+                </ul>
+                <img
+                  style={{ width: 100 }}
+                  src={e.flags.png}
+                  alt="country flag"
+                />
+              </div>
+            );
+          })
+        ) : (
+          filterCountries.map((e) => <p key={e.cca3}>{e.name.common}</p>)
+        )
+      ) : (
+        <p>Too many matches, specify another filter</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
